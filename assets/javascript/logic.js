b9b2e4230cfc57508874d022bf8c40b6bf5fbfe4
baysik <iambaysik@gmail.com>
2019-06-18 16:12:18 -0600

    created logic for firebase api

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBsesCbeSBjDSsvQ8UqU9P9DEKhZCo5UPs",
-    authDomain: "trainscheduler-3f222.firebaseapp.com",
-    databaseURL: "https://trainscheduler-3f222.firebaseio.com",
-    projectId: "trainscheduler-3f222",
-    storageBucket: "trainscheduler-3f222.appspot.com",
-    messagingSenderId: "88806088749",
-    appId: "1:88806088749:web:7c8366ac777369d7"
+apiKey: "AIzaSyBsesCbeSBjDSsvQ8UqU9P9DEKhZCo5UPs",
+authDomain: "trainscheduler-3f222.firebaseapp.com",
+databaseURL: "https://trainscheduler-3f222.firebaseio.com",
+projectId: "trainscheduler-3f222",
+storageBucket: "trainscheduler-3f222.appspot.com",
+messagingSenderId: "88806088749",
+appId: "1:88806088749:web:7c8366ac777369d7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -19,8 +19,39 @@ var destination = "";
 var firstTrainTime = "";
 var frequency = "";
 // capture button click
-$("#submit-button").click(function(event){
+$("#add-train-button").on("click", function(event) {
     event.preventDefault();
     // logic for storing and retrieving train paramaters
-    
+    trainName = $("#train-name-form").val().trim();
+    destination = $("#destination-form").val();
+    firstTrainTime = $("#train-time-form").val();
+    frequency = $("#frequency-form").val();
+    // push code to database
+    dataRef.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+// firebase watcher
+dataRef.ref().on("child_added", function(childSnapshot){
+
+    // log everything fromt he snapshot
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().firstTrainTime);
+    console.log(childSnapshot.val().frequency);
+
+    // append to table
+    $("tbody").append("<tr><td scope='row'>" + childSnapshot.val().trainName);
 })
+
+dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+    $("train-name-form").text(snapshot.val().trainName);
+    $("destination-form").text(snapshot.val().destination);
+    $("train-time-form").text(snapshot.val().firstTrainTime);
+    $("frequency-form").text(snapshot.val().frequency);
+});
